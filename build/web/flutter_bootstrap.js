2cb04c51a4309e61e4bba6d589d3ce6f10a1fae0
mediBOnetwork <masteromprakashsahu@gmail.com>
2026-06-04 00:36:58 +0000

    update

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "110846203" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */,
+    serviceWorkerVersion: "2531423751" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */,
   },
 });

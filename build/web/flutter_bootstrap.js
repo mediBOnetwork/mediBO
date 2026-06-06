91806adbcb39117d7d11317775c4196e1087c428
mediBOnetwork <masteromprakashsahu@gmail.com>
2026-06-06 04:00:48 +0000

    CHANGE #32b: force remove Home from admin nav

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -44,7 +44,7 @@ _flutter.loader.load({
     useLocalCanvasKit: false,
   },
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3788242254" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */,
+    serviceWorkerVersion: "744344340" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */,
   },
   onEntrypointLoaded: async (engineInitializer) => {
     const appRunner = await engineInitializer.initializeEngine({});

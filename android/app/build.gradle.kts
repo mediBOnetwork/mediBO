790cfdb46cd3b8e93ed1634f7c9a1118a2676741
Ubuntu <ubuntu@ip-172-31-41-212.ap-south-1.compute.internal>
2026-08-23 12:50:25 +0000

    CHANGE #806: deploy

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -36,8 +36,8 @@ android {
         applicationId = "in.medibo.app"
         minSdk = flutter.minSdkVersion
         targetSdk = 36
-        versionCode = 27
-        versionName = "1.3.14"
+        versionCode = 28
+        versionName = "1.3.15"
     }
 
     signingConfigs {
